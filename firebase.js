0d0a87c0c61cd2f0f35b74547db356df92ec2daf
Adish Pathare <adish.pathare@gmail.com>
2021-04-06 18:05:33 +0530

    firebase javascript

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBnAnjOnrX9w8BRqaPNSiUrj3RvnQ_ZvsA",
+    authDomain: "number-game-446a8.firebaseapp.com",
+    projectId: "number-game-446a8",
+    storageBucket: "number-game-446a8.appspot.com",
+    databaseURL: "https://number-game-446a8-default-rtdb.firebaseio.com/",
+    messagingSenderId: "1073849575497",
+    appId: "1:1073849575497:web:29ea428cf46d991e8f3798"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
